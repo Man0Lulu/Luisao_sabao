b1c3e10526554537fa97dea197ab6f74d3775261
Luís Nascimento <luisrocksousa1111@gmail.com>
2024-12-13 11:57:51 -0300

    adicionando funcionalidades para a barra de navegação

---
+++ pesquisa.js
@@ -0,0 +1,17 @@
+function envio() {
+    var texto = window.document.getElementById('form');
+    var campo = texto.value.toLowerCase(); 
+
+    if (campo == "detergente" || campo == "detergentes") {
+        window.location.href = "quimicos.html";
+    } else if (campo == "cabelo" || campo == "shampo" || campo == "shampoo" || campo == "condicionador"
+        || campo == "qboa" || campo == "desinfetante" || campo == "multiuso" || campo == "amaciante"
+        || campo == "sabão em pó" || campo == "sabao em po"){
+        window.location.href = "pessoal.html";
+    } else if (campo == "equipamento" || campo == "vassoura" || campo == "rodo" || campo == "luva de borracha" 
+        || campo == "escova" || campo == "balde" || campo == "esponja" || campo == "rodo pequeno"){
+        window.location.href = "ferramentas.html";
+    } else {
+        alert("Nenhum resultado encontrado."); 
+    }
+}
